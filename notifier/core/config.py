ee4bdacb90253895fa9cfd0aca44e1b9cb3e1e16
Felipe Ortiz <fortizc@gmail.com>
2017-11-21 00:59:54 -0300

    Move constant as a private memeber to hide implementation

--- notifier/core/config.py
+++ notifier/core/config.py
@@ -5,13 +5,12 @@ from .private.singleton import Singleton
 
 @Singleton
 class Config:
-    NOTIFIER_CONFIG = "NOTIFIER_CONFIG"
-
     def __init__(self):
-        self.__path = os.environ.get(Config.NOTIFIER_CONFIG, "")
+        self.__NOTIFIER_CONFIG = "NOTIFIER_CONFIG"
+        self.__path = os.environ.get(self.__NOTIFIER_CONFIG, "")
 
         if not self.__path:
-            raise RuntimeError(Config.NOTIFIER_CONFIG + " not defined")
+            raise RuntimeError(self.__NOTIFIER_CONFIG + " not defined")
 
         self.__conf = {}
 
