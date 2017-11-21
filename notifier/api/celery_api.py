809d8e575bd3ba92c0a604a18312c190bbf448d8
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-11-21 09:45:01 -0300

    Add a simple example to use celery logs

--- notifier/api/celery_api.py
+++ notifier/api/celery_api.py
@@ -1,8 +1,11 @@
+from celery.utils.log import get_task_logger
 from ..core.celery import NotifierApp
 
 app = NotifierApp.instance()
+log = get_task_logger(__name__)
 
 
 @app.task(queue="notifier_queue")
 def add(x, y):
+    log.info("Starting task with: %s" % locals())
     return x + y
