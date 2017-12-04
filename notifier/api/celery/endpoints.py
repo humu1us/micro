1766271dce300ba9902b982b03b673e62d47f0fc
Felipe Ortiz <fortizc@gmail.com>
2017-12-04 01:53:03 -0300

    Remove ApiImpl class

--- notifier/api/celery/endpoints.py
+++ notifier/api/celery/endpoints.py
@@ -1,24 +1,26 @@
 from ...core.notifierapp import NotifierApp
 from ...helper.celeryapi import celery_name
 from ...helper.celeryapi import queue
-from ...core.apiimpl import ApiImpl
+from ...plugin.pluginloader import PluginLoader
 
 
 app = NotifierApp.instance()
-api = ApiImpl()
+plugin_loader = PluginLoader()
 QUEUE = queue()
 
 
 @app.task(name=celery_name("plugin_list"), queue=QUEUE)
 def plugin_list():
-    return api.plugin_list()
+    return plugin_loader.list_all()
 
 
 @app.task(name=celery_name("plugin_info"), queue=QUEUE)
 def plugin_info(name):
-    return api.plugin_info(name)
+    return plugin_loader.long_description(name)
 
 
 @app.task(name=celery_name("notify"), queue=QUEUE)
-def notify():
-    return api.notify()
+def notify(plugin_name, **kwargs):
+    plg = plugin_loader.instance(plugin_name)
+    result = plg.run(**kwargs)
+    return result
