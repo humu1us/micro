c0debb9e13427982be807777b327f447334e146e
Felipe Ortiz <fortizc@gmail.com>
2017-12-06 00:50:24 -0300

    Remove singleton from Config and move data to NotifierApp

--- notifier/api/celery/endpoints.py
+++ notifier/api/celery/endpoints.py
@@ -1,26 +1,23 @@
 from ...core.notifierapp import NotifierApp
 from ...helper.celeryapi import celery_name
-from ...helper.celeryapi import queue
-from ...helper.celeryapi import plugin_path
 from ...plugin.pluginmanager import PluginManager
 
 
 app = NotifierApp()
-manager = PluginManager(plugin_path())
-QUEUE = queue()
+manager = PluginManager(app.plugin_path())
 
 
-@app.task(name=celery_name("list"), queue=QUEUE)
+@app.task(name=celery_name("list"), queue=app.queue())
 def list():
     return manager.list()
 
 
-@app.task(name=celery_name("info"), queue=QUEUE)
+@app.task(name=celery_name("info"), queue=app.queue())
 def info(name):
     return manager.info(name)
 
 
-@app.task(name=celery_name("run"), queue=QUEUE)
+@app.task(name=celery_name("run"), queue=app.queue())
 def run(plugin_name, **kwargs):
     plg = manager.instance(plugin_name)
     result = plg.run(**kwargs)
--- notifier/helper/celeryapi.py
+++ notifier/helper/celeryapi.py
@@ -5,11 +5,3 @@ NAMESPACE = "Notifier"
 
 def celery_name(function_name):
     return NAMESPACE + "." + function_name
-
-
-def queue():
-    return Config.instance().key("queue_name")
-
-
-def plugin_path():
-    return Config.instance().key("plugin_path")
