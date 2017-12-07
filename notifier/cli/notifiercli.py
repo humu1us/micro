610c931027578e772e9150a39833466c40c4f0a7
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-12-07 00:00:10 -0300

    Add the 'args' function to get the cli arguments

--- notifier/cli/notifiercli.py
+++ notifier/cli/notifiercli.py
@@ -1,14 +1,48 @@
 import argparse
 
+
 def args():
     args = argparse.ArgumentParser(epilog=__doc__)
 
-    help_app = "Choose between Celery, Flask or both. Default is both"
-    args.add_argument("app",
-                      default="all",
-                      const="all",
-                      nargs="?",
-                      choices=["celery", "flask", "all"],
-                      help=help_app)
+    args.add_argument("-p",
+                      "--plugin_path",
+                      required=False,
+                      help="Set the plugin path")
+
+    args.add_argument("-b",
+                      "--broker_url",
+                      required=False,
+                      help="Set the broker url")
+
+    args.add_argument("-q",
+                      "--queue_name",
+                      required=False,
+                      help="Set the Celery queue name")
+
+    args.add_argument("-H",
+                      "--hostname",
+                      required=False,
+                      help="Set the hostname for the workers")
+
+    args.add_argument("-w",
+                      "--num_workers",
+                      type=int,
+                      required=False,
+                      help="Set the Celery worker number")
+
+    args.add_argument("-l",
+                      "--log_from",
+                      required=False,
+                      help="Set the logger level")
+
+    args.add_argument("-lf",
+                      "--log_file",
+                      required=False,
+                      help="Set the log file path")
+
+    args.add_argument("-pf",
+                      "--pid_file",
+                      required=False,
+                      help="Set the pid file path")
 
     return args.parse_args()
