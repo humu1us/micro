8abd2df906fa08eceab18afaaf191631e25859ba
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-12-07 02:26:52 -0300

    Add a new cli class to handle the cli arguments
    So the before function added in the cli folder has been deleted.

---
+++ notifier/core/cli.py
@@ -0,0 +1,42 @@
+from argparse import ArgumentParser
+
+
+class CLI(ArgumentParser):
+    def __init__(self):
+        super().__init__()
+        self.__parse_args()
+
+    def __parse_args(self):
+        self.add_argument("-p",
+                          "--plugin_path",
+                          required=False,
+                          help="Set the plugin path")
+        self.add_argument("-b",
+                          "--broker_url",
+                          required=False,
+                          help="Set the broker url")
+        self.add_argument("-q",
+                          "--queue_name",
+                          required=False,
+                          help="Set the Celery queue name")
+        self.add_argument("-H",
+                          "--hostname",
+                          required=False,
+                          help="Set the hostname for the workers")
+        self.add_argument("-w",
+                          "--num_workers",
+                          type=int,
+                          required=False,
+                          help="Set the Celery worker number")
+        self.add_argument("-l",
+                          "--log_from",
+                          required=False,
+                          help="Set the logger level")
+        self.add_argument("-lp",
+                          "--log_path",
+                          required=False,
+                          help="Set the log file path")
+        self.add_argument("-pp",
+                          "--pid_path",
+                          required=False,
+                          help="Set the pid file path")
