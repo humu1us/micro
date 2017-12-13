3eee84e1ccc3d96551e48532fee35aaedcd51629
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-12-12 23:42:55 -0300

    The 'dest' args is removed because it is not needed

--- notifier/core/cli.py
+++ notifier/core/cli.py
@@ -10,17 +10,14 @@ class CLI(ArgumentParser):
         self.add_argument("-p",
                           "--plugin-path",
                           required=False,
-                          dest="plugin_path",
                           help="Set the plugin path")
         self.add_argument("-b",
                           "--broker-url",
                           required=False,
-                          dest="broker_url",
                           help="Set the broker url")
         self.add_argument("-q",
                           "--queue-name",
                           required=False,
-                          dest="queue_name",
                           help="Set the Celery queue name")
         self.add_argument("-H",
                           "--hostname",
@@ -30,25 +27,20 @@ class CLI(ArgumentParser):
                           "--num-workers",
                           type=int,
                           required=False,
-                          dest="num_workers",
                           help="Set the Celery worker number")
         self.add_argument("-l",
                           "--log-from",
                           required=False,
-                          dest="log_from",
                           help="Set the logger level")
         self.add_argument("-lp",
                           "--log-path",
                           required=False,
-                          dest="log_path",
                           help="Set the log file path")
         self.add_argument("-pp",
                           "--pid-path",
                           required=False,
-                          dest="pid_path",
                           help="Set the pid file path")
         self.add_argument("--default-params",
                           default=False,
                           action="store_true",
-                          dest="default_params",
                           help="Show default parameters")
