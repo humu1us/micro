ff407475cb5297ac07820426f7515be5c5e97b19
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-12-12 23:36:11 -0300

    Save args variables independently from the arg names

--- notifier/core/cli.py
+++ notifier/core/cli.py
@@ -8,39 +8,47 @@ class CLI(ArgumentParser):
 
     def __parse_args(self):
         self.add_argument("-p",
-                          "--plugin_path",
+                          "--plugin-path",
                           required=False,
+                          dest="plugin_path",
                           help="Set the plugin path")
         self.add_argument("-b",
-                          "--broker_url",
+                          "--broker-url",
                           required=False,
+                          dest="broker_url",
                           help="Set the broker url")
         self.add_argument("-q",
-                          "--queue_name",
+                          "--queue-name",
                           required=False,
+                          dest="queue_name",
                           help="Set the Celery queue name")
         self.add_argument("-H",
                           "--hostname",
                           required=False,
                           help="Set the hostname for the workers")
         self.add_argument("-w",
-                          "--num_workers",
+                          "--num-workers",
                           type=int,
                           required=False,
+                          dest="num_workers",
                           help="Set the Celery worker number")
         self.add_argument("-l",
-                          "--log_from",
+                          "--log-from",
                           required=False,
+                          dest="log_from",
                           help="Set the logger level")
         self.add_argument("-lp",
-                          "--log_path",
+                          "--log-path",
                           required=False,
+                          dest="log_path",
                           help="Set the log file path")
         self.add_argument("-pp",
-                          "--pid_path",
+                          "--pid-path",
                           required=False,
+                          dest="pid_path",
                           help="Set the pid file path")
-        self.add_argument("--default_params",
+        self.add_argument("--default-params",
                           default=False,
-                          action='store_true',
+                          action="store_true",
+                          dest="default_params",
                           help="Show default parameters")
