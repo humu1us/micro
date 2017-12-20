04dc97973fc0f57754826e0c749ddfe736ce8f0a
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-12-20 00:39:11 -0300

    Update test priority method

--- tests/core/test_params.py
+++ tests/core/test_params.py
@@ -1,27 +1,32 @@
-from os import path
-from os import environ
+import os
 from unittest import TestCase
 from micro.core.params import Params
 
 
 class TestParams(TestCase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.parent = path.abspath(path.join(path.dirname(__file__),
-                                             path.pardir))
-        self.file = path.join(self.parent, "test_config.json")
-        environ["MICRO_CONFIG"] = self.file
-
-        environ["MICRO_HOSTNAME"] = "env_var_hostname"
-
     def test_priority(self):
+        os.environ["MICRO_HOSTNAME"] = "env_var_hostname"
+        self.__set_config_env()
+
+        params = Params()
+        params._Params__args = params._Params__cli.parse_args(
+            ["-H", "cli_hostname"])
+        self.assertEqual(params.hostname(), "cli_hostname")
+
         params = Params()
         self.assertEqual(params.hostname(), "env_var_hostname")
 
-        del environ["MICRO_HOSTNAME"]
+        del os.environ["MICRO_HOSTNAME"]
         params = Params()
         self.assertEqual(params.hostname(), "config_hostname")
 
-        del environ["MICRO_CONFIG"]
+        del os.environ["MICRO_CONFIG"]
         params = Params()
-        self.assertEqual(params.hostname(), "micro")
+        self.assertEqual(params.hostname(),
+                         params._Params__default["hostname"])
+
+    def __set_config_env(self):
+        self.parent = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                   os.path.pardir))
+        self.file = os.path.join(self.parent, "test_config.json")
+        os.environ["MICRO_CONFIG"] = self.file
