de87f6e79801fb31c93137f0776f5413fa0d4bc0
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-12-20 00:46:22 -0300

    Add 'test_default_arg' and 'test_all_params' methods

--- tests/core/test_params.py
+++ tests/core/test_params.py
@@ -1,4 +1,7 @@
 import os
+import io
+import sys
+import json
 from unittest import TestCase
 from micro.core.params import Params
 
@@ -24,6 +27,38 @@ class TestParams(TestCase):
         params = Params()
         self.assertEqual(params.hostname(),
                          params._Params__default["hostname"])
+
+    def test_default_arg(self):
+        stdout = sys.stdout
+        sys.stdout = io.StringIO()
+
+        params = Params()
+        params._Params__args = params._Params__cli.parse_args(
+            ["--default-params"])
+        with self.assertRaises(SystemExit):
+            params._Params__check_default()
+
+        output = sys.stdout.getvalue()
+        sys.stdout = stdout
+        self.assertDictEqual(json.loads(output), params._Params__default)
+
+    def test_all_params(self):
+        del os.environ["MICRO_CONFIG"]
+        params = Params()
+        self.assertEqual(params.broker_url(),
+                         params._Params__default["broker_url"])
+        self.assertEqual(params.queue_name(),
+                         params._Params__default["queue_name"])
+        self.assertEqual(params.hostname(),
+                         params._Params__default["hostname"])
+        self.assertEqual(params.num_workers(),
+                         params._Params__default["num_workers"])
+        self.assertEqual(params.log_from(),
+                         params._Params__default["log_from"])
+        self.assertEqual(params.log_path(),
+                         params._Params__default["log_path"])
+        self.assertEqual(params.pid_path(),
+                         params._Params__default["pid_path"])
 
     def __set_config_env(self):
         self.parent = os.path.abspath(os.path.join(os.path.dirname(__file__),
