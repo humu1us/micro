c2a9118ff2f084b89a1fa5e9bb79d060efcbd438
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-12-28 02:36:23 -0300

    Update version to '1.0.0'

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='Micro',
 
-    version='0.0.1',
+    version='1.0.0',
 
     description='Celery platform to create microservices',
     long_description=long_description,
