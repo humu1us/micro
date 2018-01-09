6844a9707b12b4984174baab56f44663da142e8e
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2018-01-09 19:57:50 -0300

    Add pep8 test for micro code and tests

---
+++ tests/test_code_format.py
@@ -0,0 +1,37 @@
+import os
+import pep8
+from unittest import TestCase
+
+
+class TestCodeFormat(TestCase):
+    def test_pep8_code(self):
+        style = pep8.StyleGuide()
+        style.options.max_line_length = 80
+        filenames = []
+        parent = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                              os.path.pardir))
+        path = os.path.join(parent, "micro")
+        for root, _, files in os.walk(path):
+            python_files = [f for f in files if f.endswith('.py')]
+            for pyfile in python_files:
+                filename = "{0}/{1}".format(root, pyfile)
+                filenames.append(filename)
+        check = style.check_files(filenames)
+        self.assertEqual(check.total_errors,
+                         0,
+                         "PEP8 style errors: %d" % check.total_errors)
+
+    def test_pep8_tests(self):
+        style = pep8.StyleGuide()
+        style.options.max_line_length = 80
+        filenames = []
+        path = os.path.abspath(os.path.dirname(__file__))
+        for root, _, files in os.walk(path):
+            python_files = [f for f in files if f.endswith('.py')]
+            for pyfile in python_files:
+                filename = "{0}/{1}".format(root, pyfile)
+                filenames.append(filename)
+        check = style.check_files(filenames)
+        self.assertEqual(check.total_errors,
+                         0,
+                         "PEP8 style errors: %d" % check.total_errors)
