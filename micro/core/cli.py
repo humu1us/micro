5dfd12d918e657b028c407028dda62b61b080f00
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2018-06-16 18:49:55 -0400

    Group CLI args in required and optionals

--- micro/core/cli.py
+++ micro/core/cli.py
@@ -1,43 +1,48 @@
 from argparse import ArgumentParser
-from argparse import REMAINDER
-from argparse import SUPPRESS
 
 
 class CLI(ArgumentParser):
     def __init__(self):
-        super().__init__()
-        self.__parse_args()
+        super().__init__(add_help=False)
+        self.description = "Micro arguments:"
+        self.__required_args()
+        self.__optional_args()
 
-    def __parse_args(self):
-        self.add_argument("-b",
-                          "--broker-url",
-                          required=False,
-                          help="Set the broker url")
-        self.add_argument("-q",
-                          "--queue-name",
-                          required=False,
-                          help="Set the Celery queue name")
-        self.add_argument("-H",
-                          "--hostname",
-                          required=False,
-                          help="Set the hostname for the workers")
-        self.add_argument("-w",
-                          "--num-workers",
-                          type=int,
-                          required=False,
-                          help="Set the Celery worker number")
-        self.add_argument("-lp",
-                          "--log-path",
-                          required=False,
-                          help="Set the log file path")
-        self.add_argument("-pp",
-                          "--pid-path",
-                          required=False,
-                          help="Set the pid file path")
-        self.add_argument("--default-params",
-                          default=False,
-                          action="store_true",
-                          help="Show default parameters")
-        self.add_argument("ignored",
-                          nargs=REMAINDER,
-                          help=SUPPRESS)
+    def __required_args(self):
+        self.req = self.add_argument_group("required arguments")
+        self.req.add_argument("-b",
+                              "--broker-url",
+                              required=False,
+                              help="RabbitMQ URL")
+        self.req.add_argument("-q",
+                              "--queue-name",
+                              required=False,
+                              help="RabbitMQ queue name")
+
+    def __optional_args(self):
+        self.opt = self.add_argument_group("optional arguments")
+        self.opt.add_argument("-H",
+                              "--hostname",
+                              required=False,
+                              help="Celery worker's hostname")
+        self.opt.add_argument("-w",
+                              "--num-workers",
+                              type=int,
+                              required=False,
+                              help="set the Celery worker number")
+        self.opt.add_argument("-lp",
+                              "--celery-log-path",
+                              required=False,
+                              help="Celery log file path")
+        self.opt.add_argument("-pp",
+                              "--celery-pid-path",
+                              required=False,
+                              help="Celery PIDs path")
+        self.opt.add_argument("--default-params",
+                              default=False,
+                              action="store_true",
+                              help="show default parameters")
+        self.opt.add_argument("-h",
+                              "--help",
+                              action="help",
+                              help="Show this help message")
