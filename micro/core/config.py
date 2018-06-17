51a67d61ad3dc0bc69163ec96590b6d2ec8580a6
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2018-06-16 22:36:55 -0400

    Update the Config class to get the path by argument

--- micro/core/config.py
+++ micro/core/config.py
@@ -1,19 +1,20 @@
-import json
 import os
+import json
+from .logger import log
 
 
 class Config:
-    def __init__(self):
-        self.__MICRO_CONFIG = "MICRO_CONFIG"
-        self.__path = os.environ.get(self.__MICRO_CONFIG, "")
+    def __init__(self, path):
         self.__conf = {}
-        if self.__path:
-            self.__load()
+        self.__load(path)
 
     def key(self, name):
         return self.__conf.get(name)
 
-    def __load(self):
-        with open(self.__path) as conf_file:
-            conf_data = conf_file.read()
-            self.__conf = json.loads(conf_data)
+    def __load(self, path):
+        if os.path.exists(path):
+            with open(path) as conf_file:
+                conf_data = conf_file.read()
+                self.__conf = json.loads(conf_data)
+        else:
+            log.error("Config file not found in {}".format(path))
