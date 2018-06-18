ecd56f052c79a999b7cedd43cc027dcd4bef04da
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2018-06-17 20:40:15 -0400

    Update to raise and error instead of save a log
    when the path doesn't exist

--- micro/core/config.py
+++ micro/core/config.py
@@ -1,20 +1,20 @@
 import os
 import json
-from .logger import log
 
 
 class Config:
-    def __init__(self, path):
+    def __init__(self, path=None):
         self.__conf = {}
-        self.__load(path)
+        if path:
+            self.__load(path)
 
     def key(self, name):
         return self.__conf.get(name)
 
     def __load(self, path):
-        if os.path.exists(path):
-            with open(path) as conf_file:
-                conf_data = conf_file.read()
-                self.__conf = json.loads(conf_data)
-        else:
-            log.error("Config file not found in {}".format(path))
+        if not os.path.exists(path):
+            raise FileNotFoundError("ERROR: config file not found in {}".format(path))
+
+        with open(path) as conf_file:
+            conf_data = conf_file.read()
+            self.__conf = json.loads(conf_data)
