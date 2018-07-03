c2ec99fef8e6fc3536bf0d9b9ea7523cea4a0687
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2018-07-03 18:48:45 -0400

    Update the Logger class
    to catch the 'gunicorn' logs
    to set a specific debug format
    to make internal attributes as privates
    include unit tests

--- micro/core/logger.py
+++ micro/core/logger.py
@@ -3,42 +3,55 @@ import logging
 from .params import Params
 from ..core.utils import set_folder
 
+FILE_NAME = "micro.log"
 LOG_FORMAT = "[%(asctime)s] %(levelname)s: " + \
     "%(message)s - %(filename)s, %(funcName)s, %(lineno)s"
+LOG_DEBUG_FORMAT = "[%(asctime)s] [%(process)d] %(levelname)s: " + \
+    "%(message)s - %(name)s, %(filename)s, %(funcName)s, %(lineno)s"
 
 
 class Logger():
     def __init__(self):
         self.__path = Params.log_path()
-        self.__level = Params.log_level()
-        self.__celery_level = Params.celery_log_level()
-        self.__formatter = logging.Formatter(fmt=LOG_FORMAT,
-                                             datefmt="%Y-%m-%d %H:%M:%S")
+        self.__level = logging.getLevelName(Params.log_level())
+        self.__micro_log = Params.namespace()
+        self.__celery_log = "celery"
+        self.__gunicorn_log = "gunicorn.error"
 
         set_folder(self.__path)
-        self.celery = logging.getLogger("celery")
-        self.celery.setLevel(self.__celery_level)
-        self.micro = logging.getLogger("Micro")
-        self.micro.setLevel(self.__level)
 
-        if not self.micro.handlers:
+        self.__micro = logging.getLogger(self.__micro_log)
+        self.__micro.setLevel(self.__level)
+        self.__celery = logging.getLogger(self.__celery_log)
+        self.__celery.setLevel(self.__level)
+        self.__gunicorn = logging.getLogger(self.__gunicorn_log)
+        self.__gunicorn.setLevel(self.__level)
+
+        datefmt = "%Y-%m-%d %H:%M:%S"
+        self.__formatter = logging.Formatter(fmt=LOG_FORMAT, datefmt=datefmt)
+        if self.__level == logging.DEBUG:
+            self.__formatter = logging.Formatter(fmt=LOG_DEBUG_FORMAT,
+                                                 datefmt=datefmt)
+
+        if not self.__micro.handlers:
             self.__set_file_handler()
 
     def __set_file_handler(self):
-        handler = logging.FileHandler(os.path.join(self.__path, "micro.log"))
+        handler = logging.FileHandler(os.path.join(self.__path, FILE_NAME))
         handler.setLevel(self.__level)
         handler.setFormatter(self.__formatter)
-        self.micro.addHandler(handler)
-        self.celery.addHandler(handler)
+        self.__micro.addHandler(handler)
+        self.__celery.addHandler(handler)
+        self.__gunicorn.addHandler(handler)
 
     def debug(self, message):
-        self.micro.debug(message)
+        self.__micro.debug(message)
 
     def info(self, message):
-        self.micro.info(message)
+        self.__micro.info(message)
 
     def warning(self, message):
-        self.micro.warning(message)
+        self.__micro.warning(message)
 
     def error(self, message):
-        self.micro.error(message)
+        self.__micro.error(message)
