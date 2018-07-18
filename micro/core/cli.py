d2200680f9b40dc6d153996cd2806b25fa3b2d91
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2018-07-17 22:01:04 -0400

    Move --gunicorn and --celery CLI args to 'services start' arg group

--- micro/core/cli.py
+++ micro/core/cli.py
@@ -5,8 +5,21 @@ class CLI(ArgumentParser):
     def __init__(self):
         super().__init__(add_help=False)
         self.description = "Micro arguments:"
+        self.__services_args()
         self.__required_args()
         self.__optional_args()
+
+    def __services_args(self):
+        msg = "start services (choose at least one)"
+        self.srv = self.add_argument_group(msg)
+        self.srv.add_argument("--celery",
+                              default=False,
+                              action="store_true",
+                              help="plugins available through Celery")
+        self.srv.add_argument("--gunicorn",
+                              default=False,
+                              action="store_true",
+                              help="plugins available through API Rest")
 
     def __required_args(self):
         self.req = self.add_argument_group("required arguments")
@@ -14,41 +27,22 @@ class CLI(ArgumentParser):
                               "--plugin-path",
                               required=False,
                               help="path to the plugins folder")
-        self.req.add_argument("-b",
-                              "--broker-url",
-                              required=False,
-                              help="RabbitMQ URL")
-        self.req.add_argument("-q",
-                              "--queue-name",
-                              required=False,
-                              help="RabbitMQ queue name")
 
     def __optional_args(self):
         self.opt = self.add_argument_group("optional arguments")
-        self.opt.add_argument("--celery",
-                              default=False,
-                              action="store_true",
-                              help="plugins available through Celery")
-        self.opt.add_argument("--gunicorn",
-                              default=False,
-                              action="store_true",
-                              help="plugins available through API Rest")
-        self.opt.add_argument("--default-params",
-                              default=False,
-                              action="store_true",
-                              help="show default parameters")
-        self.opt.add_argument("--version",
-                              default=False,
-                              action="store_true",
-                              help="show Micro version")
-        self.opt.add_argument("-h",
-                              "--help",
-                              action="help",
-                              help="Show this help message")
         self.opt.add_argument("-c",
                               "--config-file",
                               required=False,
                               help="path to the config file")
+        self.opt.add_argument("-b",
+                              "--broker-url",
+                              required=False,
+                              help="RabbitMQ URL")
+        self.opt.add_argument("-q",
+                              "--queue-name",
+                              required=False,
+                              help="RabbitMQ queue name")
+
         self.opt.add_argument("-H",
                               "--hostname",
                               required=False,
@@ -83,3 +77,15 @@ class CLI(ArgumentParser):
                               "--celery-pid-path",
                               required=False,
                               help="Celery PIDs path")
+        self.opt.add_argument("--default-params",
+                              default=False,
+                              action="store_true",
+                              help="show default parameters")
+        self.opt.add_argument("--version",
+                              default=False,
+                              action="store_true",
+                              help="show Micro version")
+        self.opt.add_argument("-h",
+                              "--help",
+                              action="help",
+                              help="Show this help message")
