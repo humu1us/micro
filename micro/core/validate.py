df45d127a5972bbd4b83af6504d7deb86686ebc4
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2018-09-30 21:50:35 -0300

    Update the folder_exist Validate class method
    to try to make the given folder if it does not exist

--- micro/core/validate.py
+++ micro/core/validate.py
@@ -9,21 +9,30 @@ class Validate:
             sys.exit("[%s] must be integer: %s" % (self.name, value))
         if value < 1:
             sys.exit("[%s] must be positive: %s" % (self.name, value))
-
         return value
 
     def file_exist(self, value):
         if value is None:
             return None
-        if not os.path.exists(value) or not os.path.isfile(value):
+        if not os.path.exists(value):
             sys.exit("[%s] file does not exists: %s" % (self.name, value))
+        if not os.path.isfile(value):
+            sys.exit("[%s] path is not a valid file: %s" % (self.name, value))
         return value
 
     def folder_exist(self, value):
         if value is None:
             return None
+        if os.path.isfile(value):
+            msg = "[%s] path name a file instead a folder: %s" % (self.name,
+                                                                  value)
+            sys.exit(msg)
         if not os.path.exists(value) or not os.path.isdir(value):
-            sys.exit("[%s] directory does not exists: %s" % (self.name, value))
+            try:
+                os.makedirs(value)
+            except Exception:
+                msg = "[%s] directory does not exists: %s" % (self.name, value)
+                sys.exit(msg)
         return value
 
     def bool(self, value):
