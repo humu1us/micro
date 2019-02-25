c138aa9b8e7e6eeca66cf96c4a7f614904422a95
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2019-02-25 18:01:27 -0300

    Update MicroApp unit tests

--- tests/core/test_microapp.py
+++ tests/core/test_microapp.py
@@ -5,6 +5,7 @@ from testfixtures import LogCapture
 from micro.core import microapp
 from micro.core.microapp import MicroApp
 from micro.core.params import Params
+from tests.utils.fakestdout import StdoutLock
 
 
 def fake_start_celery():
@@ -49,18 +50,32 @@ class TestMicroApp(TestCase):
         del os.environ["_MICRO_CELERY"]
         del os.environ["_MICRO_GUNICORN"]
         app = MicroApp()
-        with LogCapture("Micro") as logs:
-            app.start()
+        with StdoutLock() as lock:
+            with LogCapture("Micro") as logs:
+                app.start()
         no_celery = ("Micro", "WARNING", "CeleryApp not started")
         no_gunicorn = ("Micro", "WARNING", "GunicornApp not started")
         logs.check(no_celery, no_gunicorn)
+
+        celery_msg = "CeleryApp not started"
+        gunicorn_msg = "GunicornApp not started"
+        lock_msgs = lock.stdout.strip().split("\n")
+        self.assertEqual(lock_msgs[0], celery_msg)
+        self.assertEqual(lock_msgs[1], gunicorn_msg)
 
     def test_start_celery(self):
         app = MicroApp()
         start_celery_aux = microapp.start_celery
         microapp.start_celery = fake_start_celery
 
-        app._MicroApp__start_celery()
+        app._MicroApp__celery = True
+        with StdoutLock() as lock:
+            app.start()
+        celery_msg = "Starting CeleryApp"
+        gunicorn_msg = "GunicornApp not started"
+        lock_msgs = lock.stdout.strip().split("\n")
+        self.assertEqual(lock_msgs[0], celery_msg)
+        self.assertEqual(lock_msgs[1], gunicorn_msg)
 
         microapp.start_celery = start_celery_aux
 
@@ -69,6 +84,13 @@ class TestMicroApp(TestCase):
         start_gunicorn_aux = microapp.start_gunicorn
         microapp.start_gunicorn = fake_start_gunicorn
 
-        app._MicroApp__start_gunicorn()
+        app._MicroApp__gunicorn = True
+        with StdoutLock() as lock:
+            app.start()
+        celery_msg = "CeleryApp not started"
+        gunicorn_msg = "Starting GunicornApp"
+        lock_msgs = lock.stdout.strip().split("\n")
+        self.assertEqual(lock_msgs[0], celery_msg)
+        self.assertEqual(lock_msgs[1], gunicorn_msg)
 
         microapp.start_gunicorn = start_gunicorn_aux
